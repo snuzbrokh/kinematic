6e5d79950de39d854fdb82f7f982a087419eddb8
Bruno Geninatti <brunogeninatti@gmail.com>
2020-08-05 18:15:13 +0000

    module's __all__

--- kin/__init__.py
+++ kin/__init__.py
@@ -1,1 +1,7 @@
+from kin.crp import CRP
+from kin.quaternion import Quaternion
+from kin.principal_rotation import PrincipalRotation
+from kin.euler_angles import EulerAngles
+from kin import helpers
 
+__all__ = ['EulerAngles', 'PrincipalRotation', 'Quaternion', 'CRP', 'helpers']
